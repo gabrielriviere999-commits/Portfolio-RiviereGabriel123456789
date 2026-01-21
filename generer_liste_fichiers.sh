55164908443d6db629b9196a496055d77355a2a8
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-01-21 16:05:54 +0400

    Update generer_liste_fichiers.sh

--- generer_liste_fichiers.sh
+++ generer_liste_fichiers.sh
@@ -13,7 +13,7 @@ generate_index() {
         echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
         echo "<title>Liste des fichiers - ${dir}</title>"
         echo "<style>"
-        echo "body { background:white; color:black; font-family:sans-serif; font-size:18px; line-height:1; }"
+        echo "body { background:white; color:black; font-family:sans-serif; font-size:18px; line-height:1.5; }"
         echo "a { 
           color:blue;          display:inline;            /* pas inline-block → permet le retrait suspendu */
           overflow-wrap:anywhere;    /* casse les très longs mots/chemins si besoin */
